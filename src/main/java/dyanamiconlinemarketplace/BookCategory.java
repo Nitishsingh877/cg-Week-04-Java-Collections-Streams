c68963153ebc7b77d4a0cb60e6740ea04a0bf8ae
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-10 15:42:23 +0530

    [Nitish] add. added BookCategory page for Dynamic Online Marketplace.

---
+++ src/main/java/dyanamiconlinemarketplace/BookCategory.java
@@ -0,0 +1,17 @@
+package dyanamiconlinemarketplace;
+
+public class BookCategory extends Category{
+            private String genre;
+    public BookCategory(String categoryName, String genre) {
+        super("Books");
+        this.genre = genre;
+    }
+
+
+
+    @Override
+    public void displayCategoryInfo() {
+        System.out.println("Category name is " + categoryName + "and genre is " + genre);
+
+    }
+}
