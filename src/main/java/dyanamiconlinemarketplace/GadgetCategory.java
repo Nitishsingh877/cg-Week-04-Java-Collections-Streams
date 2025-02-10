6677f68f4a5260ad595fbc71e87e7ae1945de613
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-10 15:46:05 +0530

    [Nitish] add. added GadgetCategory page for Dynamic Online Marketplace.

---
+++ src/main/java/dyanamiconlinemarketplace/GadgetCategory.java
@@ -0,0 +1,21 @@
+package dyanamiconlinemarketplace;
+
+public class GadgetCategory extends Category{
+    private String deviceType;
+
+    public GadgetCategory(String categoryName,String deviceType) {
+        super("Gadget");
+        this.deviceType = deviceType;
+
+    }
+
+    @Override
+    public String getCategoryName() {
+        return super.getCategoryName();
+    }
+
+    @Override
+    public void displayCategoryInfo() {
+        System.out.println("\ncategory is " + getCategoryName() + " device type is " + deviceType);
+    }
+}
