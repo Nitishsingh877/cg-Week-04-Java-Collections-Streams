63050713ea0ac451e439b4661250b074204a5873
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-12 10:47:19 +0530

    :add: [Nitish] added code for remove duplicate using LinkedHashSet  collection and tested

---
+++ Day02-JavaCollections/src/main/java/listinterface/reversealist/findfrequencyofelements/RemoveDuplicates.java
@@ -0,0 +1,16 @@
+package listinterface.reversealist.findfrequencyofelements;
+
+import java.util.*;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        Integer[] arr = {3,1,2,2,3,4};
+        //it will sort too
+//        TreeSet<Integer> ts = new TreeSet<>(Arrays.asList(arr));
+        //it will remove duplicates and maintains insertion order.
+      LinkedHashSet<Integer> map = new LinkedHashSet<>(Arrays.asList(arr));
+
+        System.out.println(map);
+    }
+}
---
+++ Day02-JavaCollections/src/test/java/listinterface/reversealist/findfrequencyofelements/RemoveDuplicatesTest.java
@@ -0,0 +1,25 @@
+package listinterface.reversealist.findfrequencyofelements;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesTest
+{
+
+
+    @Test
+    void testForDuplicatesRemove(){
+        Integer[] arr = {3,1,2,2,3,4};
+        LinkedHashSet<Integer> map = new LinkedHashSet<>(Arrays.asList(arr));
+
+     LinkedHashSet<Integer> expected = new LinkedHashSet<>(Arrays.asList(3,1,2,4));
+
+        assertEquals(expected,map,"not matched");
+        System.out.println("test case passed");
+    }
+}
