6729a9cafd3f3e3abc4c4c1203f332492c9e7b39
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-12 10:37:13 +0530

    :add: [Nitish] added code for rotate element by any index using rotate keyword of collection and tested

---
+++ Day02-JavaCollections/src/test/java/listinterface/reversealist/findfrequencyofelements/RotateElementInListTest.java
@@ -0,0 +1,26 @@
+package listinterface.reversealist.findfrequencyofelements;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RotateElementInListTest {
+    @Test
+    void testForRotate(){
+        Integer[] n  ={10,20,30,40,50};
+        List<Integer> al = Arrays.asList(n);
+        int rotateBy = 2;
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(30,40,50,10,20));
+        //rotate using rotate method.
+        Collections.rotate(al,-rotateBy);
+
+        assertEquals(expected,al,"not expected output");
+        System.out.println("test case passed!!");
+    }
+
+}
