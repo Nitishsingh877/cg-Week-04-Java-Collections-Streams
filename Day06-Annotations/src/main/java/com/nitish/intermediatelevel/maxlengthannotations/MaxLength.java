020d54428a09ba75d3313af6a19575965861adbe
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-17 23:48:12 +0530

    :👍: [Nitish] .ADD added code for max length annotations using @MaxLength annotations.

---
+++ Day06-Annotations/src/main/java/com/nitish/intermediatelevel/maxlengthannotations/MaxLength.java
@@ -0,0 +1,11 @@
+package com.nitish.intermediatelevel.maxlengthannotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MaxLength {
+    int value();
+}
